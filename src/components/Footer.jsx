4deeec143c966df1d63e35626c33b76b242b4599
lol <pranav.ppv23@gmail.com>
2023-03-29 19:00:17 +0530

    minor changes to footer and navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,15 +15,14 @@ const Footer = () => {
       <h1 className="text-3xl">CONTACT US!<br/></h1>
       <h1 className='w-full text-3xl font-bold text-black'>Entrepreneurship Club PES University ECC</h1>
       <p className='py-4'>Welcome to the official site of The Entrepreneurship Club PES University ECC<br/><br/> Feel free to check out our recources and follow us on the Social Media platforms below</p>
-      <div className='flex justify-between md:w-[75%] my-6'>
-          
+      <div className='flex justify-between md:w-[75%] '>
           <a href="https://www.instagram.com/entrepreneurshipclub.pes/"><FaInstagram size={30} className="cursor-pointer hover:opacity-70 hover:text-orange-500" /></a>
           <a href=""><FaTwitterSquare size={30} className="cursor-pointer hover:opacity-7 hover:text-orange-5000 hover:text-orange-500" /></a>
           <a href="https://github.com/EntrepreneurshipClubPES"><FaGithubSquare size={30} className="cursor-pointer hover:opacity-70 hover:text-orange-500" /></a>
       </div>
     </div>
     <div className='lg:col-span-2 flex justify-between mt-6'>
-  <div className='w-[325px]'>
+  <div className='m-6'>
       <h6 className='text-3xl text-black cursor-pointer hover:opacity-70'>Visit Us!</h6>
       <ul>
           <li className='py-2 text-sm cursor-pointer hover:opacity-70 hover:text-orange-500'>The Entrepreneurship Club, PES University ECC <br/>VM67+HVP, Hosur Rd, Konappana Agrahara, Electronic City, Bengaluru, Karnataka 560100</li>
